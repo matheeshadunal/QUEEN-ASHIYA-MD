7ddce270aceb8bfc864229f9854e1160e599bb2a
Maleesha dilukshan <133895520+matheeshadunal@users.noreply.github.com>
2024-08-22 15:45:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,24 +22,24 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.owner = ['94786746412'] //your owner number
-global.premium = ['94786746412'] //your Owner number in menu msg
-global.ownernomer = '94786746412' //your owner number info
+global.owner = ['94756965678'] //your owner number
+global.premium = ['94756965678'] //your Owner number in menu msg
+global.ownernomer = '94756965678' //your owner number info
 
-global.ownername = 'Ishan Sandeepa' //your owner name
-global.botname = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞' //your bot name    // ඔයා බොට්ගේ නම නම් වෙනස් කලාට වැඩක් නෑ... බොට්ගේ නම ( 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞 ) තමයි...
-global.footer = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞' 
+global.ownername = '𝙈𝘼𝙇𝙀𝙀𝙔𝘼 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃 .........🖤' //your owner name
+global.botname = '𝙈𝘼𝙇𝙀𝙀𝙔𝘼 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃 .........🖤' //your bot name    // ඔයා බොට්ගේ නම නම් වෙනස් කලාට වැඩක් නෑ... බොට්ගේ නම ( 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞 ) තමයි...
+global.footer = '𝙈𝘼𝙇𝙀𝙀𝙔𝘼 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃 .........🖤' 
 
 global.ig = 'https://github.com/ishansandeepa757' //Github Account!
 global.region = 'Sri Lanka'
 global.sc = 'https://github.com/ishansandeepa757'
 global.myweb = 'https://www.youtube.com/c/SinhanadaMusicRemix' //your chanal link
-global.packname = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞'
+global.packname = '𝙈𝘼𝙇𝙀𝙀𝙔𝘼 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃 .........🖤'
 
 global.chatbotpre = "true"
 
-global.author = '𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺:- 𝘐𝘴𝘩𝘢𝘯 𝘚𝘢𝘯𝘥𝘦𝘦𝘱𝘢'
-global.sessionName = 'session'
+global.author = '𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺:- maleeya'
+global.sessionName = 'bkFwS2Zqd1c='
 global.prefa = ['','!','.','🐦','🐤','🗿']                          // 👈 මේවා ඔයාට ඕන විදියට වෙනස් කර ගන්න \\ 
 global.sp = '⭔'
 
